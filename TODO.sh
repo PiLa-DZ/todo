f56a869f9719b5d3249662c5b058f5736b6dee28
Nabil <Nabil@gmail.com>
2026-02-02 22:10:52 +0100

    Day 33

--- TODO.sh
+++ TODO.sh
@@ -1,1 +1,15 @@
-- [ ] Database_Administration_DBA
+
+02. [ ] ACID
+04. [ ] Failure Modes
+05. [ ] Profiling Performance
+02. [ ] PostgreSQL
+
+01. [x] MySQL
+03. [x] Migrations
+04. [x] N+1 Problem
+01. [x] Transactions
+03. [x] Normalization
+18. [x] Scaling Databases
+    01. [x] Database Indexes
+
+
